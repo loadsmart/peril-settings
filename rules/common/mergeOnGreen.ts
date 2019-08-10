cc5d44956dd13fc738d08766e3c3f7fc4b4ddaf2
Oliver Hung Buo Tso <olivertsor@gmail.com>
2019-08-10 00:04:03 -0300

    Handle check_suite.completed in merge-on-green

--- rules/common/mergeOnGreen.ts
+++ rules/common/mergeOnGreen.ts
@@ -1,27 +1,32 @@
 import { danger } from "danger"
-import { Status } from "github-webhook-event-types"
 import { LabelLabel } from "github-webhook-event-types/source/Label"
 
-const mergeOnGreen = async (status: Status) => {
+const mergeOnGreen = async () => {
   const api = danger.github.api
 
-  if (status.state !== "success") {
-    return console.info(
-      `Not a successful state for merging (note that you can define state in the settings.json) - got ${status.state}`
-    )
+  let ref
+  if (danger.github.action === "completed" && danger.github.check_suite) {
+    console.info("Got check_suite.completed.")
+    ref = danger.github.check_suite.head_sha
+  } else if (danger.github.state === "success" && danger.github.commit) {
+    console.info("Got status.success.")
+    ref = danger.github.commit.sha
+  } else {
+    return console.info("Got neither check_suite.completed nor status.success, skipping.")
   }
 
+  const owner = danger.github.repository.owner.login
+  const repo = danger.github.repository.name
+
   // Check to see if all other statuses on the same commit are also green. E.g. is this the last green.
-  const owner = status.repository.owner.login
-  const repo = status.repository.name
-  const allGreen = await api.repos.getCombinedStatusForRef({ owner, repo, ref: status.commit.sha })
+  const allGreen = await api.repos.getCombinedStatusForRef({ owner, repo, ref })
   if (allGreen.data.state !== "success") {
     return console.info("Not all statuses are green for merging.")
   }
 
   // See https://github.com/maintainers/early-access-feedback/issues/114 for more context on getting a PR from a SHA
-  const repoString = status.repository.full_name
-  const searchResponse = await api.search.issues({ q: `${status.commit.sha} type:pr is:open repo:${repoString}` })
+  const repoString = danger.github.repository.full_name
+  const searchResponse = await api.search.issues({ q: `${ref} type:pr is:open repo:${repoString}` })
 
   // https://developer.github.com/v3/search/#search-issues
   const prsWithCommit = searchResponse.data.items.map((i: any) => i.number) as number[]
@@ -38,7 +43,7 @@ const mergeOnGreen = async (status: Status) => {
     // Merge the PR
     try {
       await api.pullRequests.merge({ owner, repo, number, commit_title: "Merged by Peril" })
-      console.log(`Merged Pull Request ${number}`)
+      return console.info(`Merged Pull Request ${number}`)
     } catch (e) {
       console.error("Error merging PR:")
       console.error(e)
