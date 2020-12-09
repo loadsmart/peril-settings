2e83bef93604463d57111fbdeb9d85ec63e27980
Camila Maia <cmaiacd@gmail.com>
2020-12-09 18:37:53 -0300

    commentDocPreviewLink rule

---
+++ rules/common/__tests__/commentDocPreviewLink.test.ts
@@ -0,0 +1,20 @@
+jest.mock("danger", () => jest.fn())
+import * as danger from "danger"
+const dm = danger as any
+
+import commentDocPreviewLink from "../commentDocPreviewLink"
+
+beforeEach(() => {
+  dm.message = jest.fn()
+})
+
+it("comment the doc preview link", () => {
+  dm.danger = {
+    github: { repository: { name: "my_repo" }, pr: { head: { ref: "my_branch" } } },
+  }
+
+  commentDocPreviewLink()
+  expect(dm.message).toHaveBeenCalledWith(
+    "Deploy preview available at https://docs.loadsmart.io/my_repo/my_branch/index.html"
+  )
+})
